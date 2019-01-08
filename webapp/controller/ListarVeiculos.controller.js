4460b1a1b60088db6845dbba2d8bec0fd42f71c5
P1942665066 <marcos.ribeirorv@outlook.com>
2019-01-08 19:00:25 +0000

    Alterado método para a exibição de dialogo na lista.

--- webapp/controller/ListarVeiculos.controller.js
+++ webapp/controller/ListarVeiculos.controller.js
@@ -1,13 +1,17 @@
 sap.ui.define(["sap/ui/core/mvc/Controller",
 	"sap/m/MessageBox",
-	"./Dialog",
+	'sap/m/Button',
+	// "./Dialog",
+	'sap/m/Dialog',
+	'sap/m/Text',
+	'sap/m/MessageToast',
 	"./utilities",
 	"sap/ui/core/routing/History"
-], function(BaseController, MessageBox, Dialog, Utilities, History) {
+], function (BaseController, MessageBox, Button, Dialog, Text, MessageToast, Utilities, History) {
 	"use strict";
 
 	return BaseController.extend("com.sap.build.standard.formInspecaoDeVeiculos.controller.ListarVeiculos", {
-		handleRouteMatched: function(oEvent) {
+		handleRouteMatched: function (oEvent) {
 			var oParams = {};
 
 			if (oEvent.mParameters.data.context) {
@@ -23,8 +27,8 @@ sap.ui.define(["sap/ui/core/mvc/Controller",
 			}
 
 		},
-		
-		_onPageNavButtonPress: function() {
+
+		_onPageNavButtonPress: function () {
 			var oHistory = History.getInstance();
 			var sPreviousHash = oHistory.getPreviousHash();
 			var oQueryParams = this.getQueryParameters(window.location);
@@ -37,8 +41,8 @@ sap.ui.define(["sap/ui/core/mvc/Controller",
 			}
 
 		},
-		
-		getQueryParameters: function(oLocation) {
+
+		getQueryParameters: function (oLocation) {
 			var oQuery = {};
 			var aParams = oLocation.search.substring(1).split("&");
 			for (var i = 0; i < aParams.length; i++) {
@@ -48,22 +52,86 @@ sap.ui.define(["sap/ui/core/mvc/Controller",
 			return oQuery;
 
 		},
-		
-		_onStandardListDelete: function(oEvent) {
-			
-			var sDialogName = "Dialog";
-			this.mDialogs = this.mDialogs || {};
-			var oDialog = this.mDialogs[sDialogName];
 
-			if (!oDialog) {
-				oDialog = new Dialog(this.getView());
-				this.mDialogs[sDialogName] = oDialog;
+		// deletaItem: function (oEvent) {
+		// 	var oList = oEvent.getSource(),
+		// 		oItem = oEvent.getParameter("listItem"),
+		// 		sPath = oItem.getBindingContext().getPath();
 
-				// For navigation.
-				oDialog.setRouter(this.oRouter);
-			}
-			oDialog.open();
-			
+		// 	// after deletion put the focus back to the list
+		// 	oList.attachEventOnce("updateFinished", oList.focus, oList);
+
+		// 	// send a delete request to the odata service
+		// 	this.oProductModel.remove(sPath);
+
+		// 	return sPath;
+		// },
+
+		_onStandardListDelete: function (oEvent) {
+
+			var that = this;
+			var oList = oEvent.getSource(),
+				oItem = oEvent.getParameter("listItem"),
+				sPath = oItem.getBindingContext().getPath();
+
+			oList.attachEventOnce("updateFinished", oList.focus, oList);
+
+			var dialog = new Dialog({
+				title: 'Confirm',
+				type: 'Message',
+
+				content: new Text({
+					text: 'Deseja deletar este cadastro de veículo?'
+				}),
+				beginButton: new Button({
+					text: 'Sim',
+					press: function () {
+						MessageToast.show('Submit pressed!');
+						dialog.close();
+					}
+				}),
+				endButton: new Button({
+					text: 'Não',
+					press: function () {
+						dialog.close();
+					}
+				}),
+				afterClose: function () {
+					dialog.destroy();
+				}
+			});
+			dialog.open();
+
+			// var dialog = new Dialog({
+			// 	title: 'Warning',
+			// 	type: 'Message',
+			// 	state: 'Warning',
+			// 	content: new Text({
+			// 		text: 'Deseja deletar este cadastro de veículo?',
+			// 		visible: true
+			// 	}),
+			// 	beginButton: new Button({
+			// 		text: 'Sim',
+			// 		press: function () {
+			// 			dialog.close();
+			// 		}
+			// 	}),
+			// 	afterClose: function () {
+			// 		dialog.destroy();
+			// 	}
+			// });
+			// dialog.open();
+
+			// var sDialogName = "Dialog";
+			// this.mDialogs = this.mDialogs || {};
+			// var oDialog = this.mDialogs[sDialogName];
+
+			// if (!oDialog) {
+			// 	oDialog = new Dialog(this.getView());
+			// 	this.mDialogs[sDialogName] = oDialog;
+			// 	oDialog.setRouter(this.oRouter);
+			// }
+			// oDialog.open();
 
 			// var sDialogName = "Dialog";
 			// this.mDialogs = this.mDialogs || {};
@@ -113,8 +181,8 @@ sap.ui.define(["sap/ui/core/mvc/Controller",
 			// });
 
 		},
-		
-		onInit: function() {
+
+		onInit: function () {
 			this.oRouter = sap.ui.core.UIComponent.getRouterFor(this);
 			this.oRouter.getTarget("ListarVeiculos").attachDisplay(jQuery.proxy(this.handleRouteMatched, this));
 		}
