58d0ce7d8f22d12de1105be216daa7720d65ff97
P1942665066 <marcos.ribeirorv@outlook.com>
2019-01-09 12:40:08 +0000

    Implementado pesquisa na lista de veículos.

--- webapp/controller/ListarVeiculos.controller.js
+++ webapp/controller/ListarVeiculos.controller.js
@@ -53,23 +53,25 @@ sap.ui.define(["sap/ui/core/mvc/Controller",
 
 		},
 
-		// deletaItem: function (oEvent) {
-		// 	var oList = oEvent.getSource(),
-		// 		oItem = oEvent.getParameter("listItem"),
-		// 		sPath = oItem.getBindingContext().getPath();
+		handleSearch: function (evt) {
+			// create model filter
+			var filters = [];
+			var query = evt.getParameter("query");
+			if (query && query.length > 0) {
+				var filter = new sap.ui.model.Filter("Placa", sap.ui.model.FilterOperator.Contains, query);
+				filters.push(filter);
+			}
 
-		// 	// after deletion put the focus back to the list
-		// 	oList.attachEventOnce("updateFinished", oList.focus, oList);
-
-		// 	// send a delete request to the odata service
-		// 	this.oProductModel.remove(sPath);
-
-		// 	return sPath;
-		// },
+			// update list binding
+			var list = this.getView().byId("listaVeiculos");
+			var binding = list.getBinding("items");
+			binding.filter(filters);
+		},
 
 		_onStandardListDelete: function (oEvent) {
 
 			var that = this;
+
 			var oList = oEvent.getSource(),
 				oItem = oEvent.getParameter("listItem"),
 				sPath = oItem.getBindingContext().getPath();
@@ -77,21 +79,42 @@ sap.ui.define(["sap/ui/core/mvc/Controller",
 			oList.attachEventOnce("updateFinished", oList.focus, oList);
 
 			var dialog = new Dialog({
-				title: 'Confirm',
-				type: 'Message',
-
+				title: "Confirmar",
+				type: "Message",
+				horizontalScrolling: true,
+				verticalScrolling: true,
+				showHeader: true,
 				content: new Text({
-					text: 'Deseja deletar este cadastro de veículo?'
+					text: "Deseja deletar este cadastro de veículo?",
+					width: "100%",
+					maxLines: 1,
+					textAlign: "Center",
+					textDirection: "Inherit",
+					visible: true
 				}),
 				beginButton: new Button({
-					text: 'Sim',
+					text: "Sim",
+					type: "Accept",
+					icon: "sap-icon://accept",
+					iconFirst: true,
+					widht: "auto",
+					enabled: true,
+					visible: true,
+					addStyleClass: "sapUiTinyMargin",
 					press: function () {
-						MessageToast.show('Submit pressed!');
+						MessageToast.show("Veículo deletado com sucesso!");
 						dialog.close();
 					}
 				}),
 				endButton: new Button({
-					text: 'Não',
+					text: "Não",
+					type: "Reject",
+					icon: "sap-icon://negative",
+					iconFirst: true,
+					widht: "auto",
+					enabled: true,
+					visible: true,
+					addStyleClass: "sapUiTinyMargin",
 					press: function () {
 						dialog.close();
 					}
@@ -101,84 +124,6 @@ sap.ui.define(["sap/ui/core/mvc/Controller",
 				}
 			});
 			dialog.open();
-
-			// var dialog = new Dialog({
-			// 	title: 'Warning',
-			// 	type: 'Message',
-			// 	state: 'Warning',
-			// 	content: new Text({
-			// 		text: 'Deseja deletar este cadastro de veículo?',
-			// 		visible: true
-			// 	}),
-			// 	beginButton: new Button({
-			// 		text: 'Sim',
-			// 		press: function () {
-			// 			dialog.close();
-			// 		}
-			// 	}),
-			// 	afterClose: function () {
-			// 		dialog.destroy();
-			// 	}
-			// });
-			// dialog.open();
-
-			// var sDialogName = "Dialog";
-			// this.mDialogs = this.mDialogs || {};
-			// var oDialog = this.mDialogs[sDialogName];
-
-			// if (!oDialog) {
-			// 	oDialog = new Dialog(this.getView());
-			// 	this.mDialogs[sDialogName] = oDialog;
-			// 	oDialog.setRouter(this.oRouter);
-			// }
-			// oDialog.open();
-
-			// var sDialogName = "Dialog";
-			// this.mDialogs = this.mDialogs || {};
-			// var oDialog = this.mDialogs[sDialogName];
-			// var oSource = oEvent.getParameter("listItem");
-			// var oBindingContext = oSource.getBindingContext();
-			// var sPath = (oBindingContext) ? oBindingContext.getPath() : null;
-			// var oView;
-			// if (!oDialog) {
-			// 	this.getOwnerComponent().runAsOwner(function() {
-			// 		oView = sap.ui.xmlview({
-			// 			viewName: "com.sap.build.standard.formInspecaoDeVeiculos.view." + sDialogName
-			// 		});
-			// 		this.getView().addDependent(oView);
-			// 		oView.getController().setRouter(this.oRouter);
-			// 		oDialog = oView.getContent()[0];
-			// 		this.mDialogs[sDialogName] = oDialog;
-			// 	}.bind(this));
-			// }
-
-			// return new Promise(function(fnResolve) {
-			// 	oDialog.attachEventOnce("afterOpen", null, fnResolve);
-			// 	oDialog.open();
-			// 	if (oView) {
-			// 		oDialog.attachAfterOpen(function() {
-			// 			oDialog.rerender();
-			// 		});
-			// 	} else {
-			// 		oView = oDialog.getParent();
-			// 	}
-
-			// 	var oModel = this.getView().getModel();
-			// 	if (oModel) {
-			// 		oView.setModel(oModel);
-			// 	}
-			// 	if (sPath) {
-			// 		var oParams = oView.getController().getBindingParameters();
-			// 		oView.bindObject({
-			// 			path: sPath,
-			// 			parameters: oParams
-			// 		});
-			// 	}
-			// }.bind(this)).catch(function(err) {
-			// 	if (err !== undefined) {
-			// 		MessageBox.error(err.message);
-			// 	}
-			// });
 
 		},
 
